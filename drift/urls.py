6fc9eeddde079d14c7c51804e4b837dd35705159
jarrod <jarrod.olson@outlook.com>
2019-12-31 10:05:58 -0800

    Adding event and event list views

--- drift/urls.py
+++ drift/urls.py
@@ -6,6 +6,8 @@ from . import api
 app_name = 'drift'
 
 urlpatterns = [
+    path('events/', views.ListEventsView.as_view(), name='listEvents'),
+    path('event/<int:pk>', views.EventView.as_view(), name='viewEvent'),
     path('api/event/', api.EventApi.as_view(), name='addEvent'),
     path('api/race/', api.RaceApi.as_view(), name='addRace'),
     path('api/racer/', api.RacerApi.as_view(), name='addRacer'),
--- drift/views.py
+++ drift/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
 from drift.models import *
 
-class UserDashboard:
+class UserDashboardView:
 
     def get(self):
         """Need to return some general links"""
 
-class League:
+class LeagueView:
 
     def get(self):
         """Should show your league, if no league redirecto to join/create"""
@@ -16,8 +17,7 @@ class League:
     def post(self):
         """"Should save a new league with corresponding rules"""
 
-
-class User:
+class UserAccountView:
 
     def get(self):
         """Should show account view"""
@@ -25,24 +25,59 @@ class User:
     def post(self):
         """Should allow user to register and save a bunch of stuff"""
 
-class Team:
+class UserTeamView:
 
     def get(self):
         """See full team and view scores for current week"""
 
-class Racer:
+    def post(self):
+        """Create a new user team"""
+
+class RacerView:
 
     def get(self):
         """See a given racer and their stats"""
 
-    def post(self):
-        """Add/update a racer"""
-
-class Event:
+class RaceTeamView:
 
     def get(self):
+        """See a given team's racers"""
+
+class ManufacturerTeamView:
+    
+    def get(self):
+        """See a given manufacturer'r racers"""
+
+class ListEventsView(View):
+
+    ##TODO: Scraper is not getting right location and dates at least in post
+
+    def get(self, request):
+        """See a list of all events"""
+
+        events = Event.objects.all()
+
+        return render(request, 'drift/eventsList.html', {'events': events})
+
+class EventView(View):
+
+    def get(self, request, pk):
         """See a given event and any plans/results"""
 
-    def post(self):
-        """Add an event"""
+        event = Event.objects.get(pk=pk)
+
+        qualify = Qualify.objects.filter(event=pk).order_by('rank')
+
+        races = Race.objects.filter(event=pk).order_by('-event_round')
+        races2 = {
+            '32': races.filter(event_round = 32),
+            '16': races.filter(event_round = 16),
+            '8': races.filter(event_round=8),
+            '4': races.filter(event_round=4),
+            '2': races.filter(event_round=2)
+        }
+
+        context = {'event': event, 'qualify': qualify, 'races': races2}
+        
+        return render(request, 'drift/event.html', context)
 
