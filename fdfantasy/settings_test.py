b7199142af0828c4be227a4d80b832a4a934284b
jarrod <jarrod.olson@outlook.com>
2020-02-23 09:05:25 -0800

    Adding smtp credentials

--- fdfantasy/settings_test.py
+++ fdfantasy/settings_test.py
@@ -2,3 +2,12 @@ from fdfantasy.settings import *
 
 STATIC_ROOT = '/var/www/fdfantasy/static/'
 MEDIA_ROOT = '/var/www/fdfantasy/media/'
+
+with open('__sensitive_emailPw.txt') as fi:
+    emailPw = fi.read().strip()
+
+EMAIl_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'AKIA4PXL5JUPU5SMBG6B'
+EMAIL_HOST_PASSWORD = emailPw
