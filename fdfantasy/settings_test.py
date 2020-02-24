22d22c7afbddebdcab6323be02ce371e98dbd047
jarrod <jarrod.olson@outlook.com>
2020-02-23 17:25:59 -0800

    trying another change to email config

--- fdfantasy/settings_test.py
+++ fdfantasy/settings_test.py
@@ -11,6 +11,7 @@ with open('__sensitive_emailPw.txt') as fi:
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'email-smtp.us-west-2.amazonaws.com'
 EMAIL_PORT = 25
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'AKIA4PXL5JUP7P25E4EW'
 EMAIL_HOST_PASSWORD = emailPw
 DEFAULT_FROM_EMAIL = 'admin@fdfantasy.com'
